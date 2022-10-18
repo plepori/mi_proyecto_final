09304b422239e6d897cf64d97167a066b30eab7b
Pablo Lepori <pablo.lepori@claro.com.ar>
2022-10-18 20:19:42 -0300

    pruebas sobre bd

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -6,4 +6,4 @@ class Familiar(models.Model):
     numero_pasaporte = models.IntegerField()
 
     def __str__(self):
-        return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
+        return f"{self.nombre},{self.direccion}, {self.numero_pasaporte}, {self.id}"
